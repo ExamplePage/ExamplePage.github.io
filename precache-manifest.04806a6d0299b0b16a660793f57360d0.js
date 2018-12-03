9d6b2808ede0df0138625ab6939d63c263dc6390
DarKCoder666 <umid010699@mail.ru>
2018-12-03 20:09:57 +0500

    TestTaskOnReact

---
+++ precache-manifest.04806a6d0299b0b16a660793f57360d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f971c34f79176c25f3ea",
+    "url": "/static/js/main.f971c34f.chunk.js"
+  },
+  {
+    "revision": "a6ca1725396bac59d54f",
+    "url": "/static/js/1.a6ca1725.chunk.js"
+  },
+  {
+    "revision": "f971c34f79176c25f3ea",
+    "url": "/static/css/main.95f70301.chunk.css"
+  },
+  {
+    "revision": "a6ca1725396bac59d54f",
+    "url": "/static/css/1.44cadd25.chunk.css"
+  },
+  {
+    "revision": "8ae0c4bfcb1a7386a3bf219a39e3b1a3",
+    "url": "/index.html"
+  }
+];
